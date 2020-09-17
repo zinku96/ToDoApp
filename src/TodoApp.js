61bd9d170628a0480e2446faf1b98a1f42607c74
zinku96 <64577769+zinku96@users.noreply.github.com>
2020-09-17 23:39:10 +0700

    Update TodoApp.js

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -5,94 +5,100 @@ class TodoApp extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      value: "",
-      listCount: 0,
+      inputAdd: "",
       list: [{
         row: "",
+        inputEdit: "",
         value: "",
-        editValue: "",
         isEditing: false,
         isDone: false
       }]
     };
-    this.handleChangeInput = this.handleChangeInput.bind(this);
-    this.handleClickButton = this.handleClickButton.bind(this);
-    this.handleRemoveButton = this.handleRemoveButton.bind(this);
+    this.handleAddButton = this.handleAddButton.bind(this);
     this.handleEditButton = this.handleEditButton.bind(this);
-    this.handleEditInput = this.handleEditInput.bind(this);
+    this.handleRemoveButton = this.handleRemoveButton.bind(this);
+    this.handleInput = this.handleInput.bind(this);
   }
-  
-  handleEditButton(event,row,action) {
-    const list = this.state.list;
-    for(let i=0; i < list.length; i++){
-      if(list[i].row === row){
-        if(action === "edit"){
-          list[i].isEditing = true;break;
-        }
-        if(action === "cancel"){
-          list[i].editValue = list[i].value;
-          list[i].isEditing = false;break;
-        }
-        if(action === "save"){
-          list[i].value = list[i].editValue;
-          list[i].isEditing = false;break;
-        }
-      }
-    }
-    this.setState({list: list});
-  }
-
-  handleEditInput(event,row){
-    const list = this.state.list;
-    let editValue = event.target.value;
-    for(let i=0; i < list.length; i++){
-      if(list[i].row === row){
-        list[i].editValue = editValue;
-      }
-    }
-    this.setState({list: list});
-  }
-  handleChangeInput(event) {
-    const value =  event.target.value;
-    this.setState({value: value});
-  }
-  handleCheckbox(event,row){
-    const list = this.state.list;
-    const value = event.target.checked;
-    for(let i=0; i < list.length; i++){
-      if(list[i].row === row){
-        list[i].isDone = value;
-      }
-    }
-    this.setState({list: list});
-  }
-  handleClickButton(event){
+  handleAddButton(event){
     event.preventDefault();
-    const list = this.state.list;
-    const value = this.state.value;
-    const currentRow = this.state.listCount;
-    if(value !== "" && value !== null){
-      let addToList = {
+    let list = this.state.list;
+    const inputAdd = this.state.inputAdd;
+    const currentRow = list.length;
+    if(inputAdd !== "" && inputAdd !== null){
+      let data = {
         row: currentRow,
-        value: value,
-        editValue: value,
-        isEditing: false
+        inputEdit: inputAdd,
+        value: inputAdd,
+        isEditing: false,
+        isDone: false
       }
-      list.push(addToList);
-      this.setState({list: list,value:'',listCount: currentRow+1});
+      list.push(data);
+      this.setState({list: list,inputAdd:''});
     }else{
       alert('Please type something');
     }
   }
+  handleEditButton(event,row,action) {
+    event.preventDefault();
+    let list = this.state.list;
+    if(action === "edit"){
+      list[row].isEditing = true;
+      this.setState({list: list});
+      return;
+    }
+    if(action === "cancel"){
+      list[row].inputEdit = list[row].value;
+      list[row].isEditing = false;
+      this.setState({list: list});
+      return;
+    }
+    if(action === "save"){
+      if(list[row].inputEdit === ""){
+        alert("Fill input!!!");
+      }else{
+        list[row].value = list[row].inputEdit;
+        list[row].isEditing = false;
+        this.setState({list: list});
+        return;
+      }
+    }
+  }
   handleRemoveButton(event,row){
     event.preventDefault();
-    const list = this.state.list;
+    let list = this.state.list;
+    let listCount = 0;
     let filters = list.filter(filter => filter.row !== row);
-    this.setState({list: filters});
+    filters.forEach(function re_index(filter){
+      filter.row = listCount;
+      listCount++;
+    });
+    this.setState({list: filters,listCount: listCount});
+  }
+  handleInput(event,row){
+    if(event.target.type === "text") {
+      if(row === ""){
+        const inputAdd =  event.target.value;
+        this.setState({inputAdd: inputAdd});
+        return;
+      }else{
+        const list = this.state.list;
+        const inputEdit = event.target.value;
+        list[row].inputEdit = inputEdit;
+        this.setState({list: list});
+        return;
+      }
+    }
+    if(event.target.type === "checkbox"){
+      let list = this.state.list;
+      const value = event.target.checked;
+      list[row].isDone = value;
+      this.setState({list: list});
+      return;
+    }
   }
   
   renderRow(){
-    const listCount = this.state.listCount;
+    const listCount = this.state.list.length;
     if(listCount > 0){ 
       const list = this.state.list;
       let returnArray = [];
@@ -101,8 +107,8 @@ class TodoApp extends React.Component{
           if (list[i].isEditing === false){
             returnArray.push(
               <tr key={list[i].row}>
-                <td><input type="checkbox"  id={list[i].row} onChange={(event) => this.handleCheckbox(event,list[i].row)} checked={list[i].isDone ? true : false}/></td>
-                <td><label for={list[i].row}>{list[i].value}</label></td>
+                <td><input checked={list[i].isDone ? true : false} type="checkbox" id={list[i].row} onChange={(event) => this.handleInput(event,list[i].row)}></input></td>
+                <td><label htmlFor={list[i].row}>{list[i].value}</label></td>
                 <td>
                   <button onClick={(event) => this.handleEditButton(event,list[i].row,"edit")}>Edit</button>
                   <button onClick={(event) => this.handleRemoveButton(event,list[i].row)}>Del</button>
@@ -113,7 +119,7 @@ class TodoApp extends React.Component{
             returnArray.push(
               <tr key={list[i].row}>
                 <td>{list[i].isDone ? "Done" : "Not Done"} </td>
-                <td><input value={list[i].editValue} onChange={(event) => this.handleEditInput(event,list[i].row)}></input></td>
+                <td><input type="text" value={this.state.list[i].inputEdit} onChange={(event) => this.handleInput(event,list[i].row)}></input></td>
                 <td>
                 <button onClick={(event) => this.handleEditButton(event,list[i].row,"save")}>Save</button>
                 <button onClick={(event) => this.handleEditButton(event,list[i].row,"cancel")}>Cancel</button>
@@ -127,20 +133,23 @@ class TodoApp extends React.Component{
       return returnArray;
     }
   }
+  
   render(){
     return (
       <div className="todo-app">
         <form className="todo-input">
-          <input type="text" placeholder="Do something?" value={this.state.value} onChange={(event) => this.handleChangeInput(event)}></input>
-          <button onClick={(event) => this.handleClickButton(event)}>Add</button>
+          <input type="text" value={this.state.inputAdd} placeholder="Do something?" onChange={(event) => this.handleInput(event,"")}></input>
+          <button onClick={(event) => this.handleAddButton(event)}>Add</button>
         </form>
         <table className="todo-list">
-          <tr>
-            <th>Done ?</th>
-            <th>Task</th>
-            <th>Action</th>
-          </tr>
-          {this.renderRow()}
+          <tbody>
+            <tr>
+              <th>Done ?</th>
+              <th>Task</th>
+              <th>Action</th>
+            </tr>
+            {this.renderRow()}
+          </tbody>
         </table>
       </div>
     )
@@ -148,3 +157,4 @@ class TodoApp extends React.Component{
 }
 
 export default TodoApp;
+
